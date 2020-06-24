9b88c7554fbe285f7a5b3a3c757dcf0646f433de
GarySimons <garycsimons@gmail.com>
2020-06-24 08:09:58 +0000

    Added Stripe to checkout. CSS not working yet

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51GxRZHHmKj7vq2Is2AWfgaboe1NuIhbktcOciMBpbLG5mPXqVccxrhrA2XfJZTSVUjuXLez6vdAmqCKiogzkvoZn00RviHujBJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
