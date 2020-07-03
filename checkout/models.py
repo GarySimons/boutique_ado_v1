d2b9c344cadf84f12a357313b9b1a95b26fb24ea
GarySimons <garycsimons@gmail.com>
2020-07-03 05:44:41 +0000

    Fixed shopping bag. Needed Stripe WH key

--- checkout/models.py
+++ checkout/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    orginal_bag = models.TextField(null=False, blank=False, default='')
+    original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
@@ -73,4 +73,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.product.sku} on order {self.order.order_number}' 
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -80,8 +80,6 @@ class StripeWH_Handler:
                     county=shipping_details.address.state,
                     original_bag=bag,
                     stripe_pid=pid,
-                    original_bag=bag,
-                    stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
