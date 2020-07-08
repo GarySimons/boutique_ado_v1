da87dda8968f84bf2be9af5003ab52382137fcf7
GarySimons <garycsimons@gmail.com>
2020-07-08 06:20:37 +0000

    Fixed user profile. Had dafaul_user, but should have been just user. Changed in models.py and migrated

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
     A user profile model for maintaining default 
     delivery information and order history
     """
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -28,6 +28,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(default_user=instance)
+        UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
